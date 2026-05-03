bbe9d984467cf6f62ba9d80946e71c579f2f5112
arnavsx3 <arnavsx3@gmail.com>
2026-05-03 15:46:39 +0530

    config socket.io server with node

--- backend/server.ts
+++ backend/server.ts
@@ -1,17 +1,30 @@
 import express from "express";
+import { createServer } from "node:http";
 import type { Request, Response } from "express";
 import cors from "cors";
+import { Server } from "socket.io";
 
 const app = express();
-const PORT = 3001;
-
 app.use(cors());
 app.use(express.json());
+
+const server = createServer(app);
+const PORT = 3001;
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
